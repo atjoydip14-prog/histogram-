e95b8beead04d7a2f5d4d9b4e58866c7d09dff6d
atjoydip14-prog <atjoydip14@gmail.com>
2025-11-23 23:06:58 -0500

    Add files via upload
    version 2

--- histogram_fitter1.py
+++ histogram_fitter1.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import stats
+import matplotlib.pyplot as plt
 import io
 import warnings
 warnings.filterwarnings("ignore")
